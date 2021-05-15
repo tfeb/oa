a92ad35442b3f02ec92d7ecd75a2432ca959bde4
Tim Bradshaw <tfb@cley.com>
2021-05-15 12:58:57 +0100

    An instrumentable version of delay & force

---
+++ private/hold.rkt
@@ -0,0 +1,60 @@
+#lang racket
+
+;;; Just a simple-minded version of delay & force
+;;; so they can be instrumented: hold is delay, release is force
+;;;
+
+(provide hold
+         (contract-out
+          (release (-> any/c any/c))
+          (release* (-> any/c any/c))
+          (hold-debugging (case->
+                            (-> boolean?)
+                            (-> boolean? any)))))
+
+(define hold-debugging (make-parameter #f))
+
+(define (dbp fmt . args)
+  (when (hold-debugging)
+    (apply eprintf fmt args)))
+
+(struct held
+  ((thing #:mutable)
+   source
+   (released? #:mutable #:auto))
+  #:methods gen:custom-write
+  ((define (write-proc it port mode)
+     ;; This is horrid manual unreadable-but-useful printing: there probably
+     ;; is a better approach to this.
+     (when mode (write-string "#<" port))
+     (fprintf port "~S" (held-source it))
+     (when mode (write-string ">" port)))))
+
+(require racket/undefined)
+
+(define-syntax-rule (hold form)
+  ;; hold a form for later releasing
+  (begin
+    (dbp "[holding ~S]~%" 'form)
+    (held (thunk form) 'form)))
+
+(define (release holder)
+  ;; release something if it is held
+  (cond [(not (held? holder))
+         (dbp "[unheld ~S]~%" holder)
+         holder]
+        [(held-released? holder)
+         (dbp "[already ~S]~%" (held-source holder))
+         (held-thing holder)]
+        [else
+         (dbp "[releasing ~S]~%" (held-source holder))
+         (let ([v ((held-thing holder))])
+           (set-held-thing! holder v)
+           (set-held-released?! holder #t)
+           v)]))
+
+(define (release* maybe-holder)
+  ;; Keep releasing until maybe-holder is not held
+  (if (held? maybe-holder)
+      (release* (release maybe-holder))
+      maybe-holder))
